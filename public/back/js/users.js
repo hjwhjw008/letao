dd68f7a0279baf4c7ef6bc6a50e54b9241e25d4b
hjw <1009949446@qq.com>
2019-09-16 00:13:31 +0800

    分页功能完成

--- public/back/js/users.js
+++ public/back/js/users.js
@@ -16,9 +16,21 @@ function rander(page, size) {
     },
     dataType: "json",
     success: function(info) {
-      console.log(info);
+      //模板动态渲染
       var htmlStr = template("tpl",info);
       $(".users-table tbody").html(htmlStr);
+      //分页模块渲染
+      $(".pagenator").bootstrapPaginator({
+        bootstrapMajorVersion: 3, //默认是2，如果是bootstrap3版本，这个参数必填
+        currentPage: currentPage, //当前页
+        totalPages: Math.ceil(info.total / info.size), //总页数 
+        size: "samll", //设置控件的大小，mini, small, normal,large
+        onPageClicked: function (event, originalEvent, type, page) {
+          //为按钮绑定点击事件 page:当前点击的按钮值
+          //点击按钮重新渲染数据
+          rander(page, size);
+        }
+      });
     }
   });
 }
